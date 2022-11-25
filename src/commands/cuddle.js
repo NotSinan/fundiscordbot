526eb94c0d43dccff876ba2fbe300f896a008993
Sinan <haxsmith77@gmail.com>
2022-11-25 21:43:52 +1100

    Added cuddle.js

---
+++ src/commands/cuddle.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
+const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName('cuddle')
+    .setDescription('Cuddle a user.')
+    .addUserOption(option => option.setName('target').setDescription("User to cuddle.") && option.setRequired(true)),
+
+    async execute(interaction) {
+
+        const data = await fetch('https://anime-api.hisoka17.repl.co/img/cuddle')
+        .then((response) => response.json())
+
+        const embed = new EmbedBuilder()
+        .setColor(0x0099FF)
+        .setImage(data.url)
+        
+        await interaction.reply({
+            content: `${interaction.user} cuddles ${interaction.options.getUser('target')}`, 
+            embeds: [embed]})
+        .catch(console.error)
+
+    }
+}
